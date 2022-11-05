298c449453c31882fbd57fa2b910cdfcb5a5757d
MoritzRS <moritz.r.schulz@gmail.com>
2022-11-05 11:26:05 +0100

    configured cmp keymaps

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -28,19 +28,19 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+		-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-		["<C-e>"] = cmp.mapping.abort(), -- close completion window
+		["<Escape>"] = cmp.mapping.abort(), -- close completion window
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" }, -- lsp
 		{ name = "luasnip" }, -- snippets
-		{ name = "buffer" }, -- text within current buffer
+		-- { name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),
 	-- configure lspkind for vs-code like icons
