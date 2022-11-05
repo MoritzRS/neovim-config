788bbf679b20fccaa4d4064676ffd861dcdb01c4
MoritzRS <moritz.r.schulz@gmail.com>
2022-11-05 17:34:15 +0100

    changed telescope layout

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -12,13 +12,11 @@ end
 
 -- configure telescope
 telescope.setup({
-	-- configure custom mappings
 	defaults = {
-		mappings = {
-			i = {
-				["<C-k>"] = actions.move_selection_previous, -- move to prev result
-				["<C-j>"] = actions.move_selection_next, -- move to next result
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
 			},
 		},
 	},
