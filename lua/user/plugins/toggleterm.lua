5ec686cab976b9c39f3387cddcd034486dd25876
MoritzRS <moritz.r.schulz@gmail.com>
2022-11-05 18:05:54 +0100

    added lazygit toggleterm

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -6,3 +6,14 @@ end
 toggleterm.setup({
     direction = "float"
 })
+
+-- Custom Lazygit toggle
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<C-g>", [[<C-\><C-N><cmd>lua _lazygit_toggle()<CR>]], {noremap = true, silent = true})
