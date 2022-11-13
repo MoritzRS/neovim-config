b8d45b123cf3a9422daba172e7e1fe8a2feaa7b8
MoritzRS <moritz.r.schulz@gmail.com>
2022-11-13 10:10:14 +0100

    minor adjustments

--- lua/user/core/autocommands.lua
+++ lua/user/core/autocommands.lua
@@ -2,9 +2,3 @@
 vim.api.nvim_create_autocmd("BufWritePre *",{
     command = "lua vim.lsp.buf.format()"
 })
-
--- Autoclose NvimTree
--- vim.api.nvim_create_autocmd('BufEnter', {
---     command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
---     nested = true,
--- })
--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -20,6 +20,7 @@ telescope.setup({
 			},
 		},
 	},
+	file_ignore_patterns = { "node_modules" },
 })
 
 telescope.load_extension("fzf")
